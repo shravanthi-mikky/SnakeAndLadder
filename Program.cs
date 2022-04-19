cadcc7cb2e3fd1fa5e376b82fe3230ca6be0a77f
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-19 11:11:08 +0530

    Initially Player is at position 0!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Snake_and_Ladder;
+
 Console.WriteLine("Welcome to Snake and Ladder Simulator!!!");
+uc1 test = new uc1();
+Console.WriteLine("Position of Player is Intiallly at : " +test.positionOfPlayer1);
---
+++ uc1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    public class uc1
+    {
+        public int positionOfPlayer1 = 0;
+
+
+    }
+    
+}
