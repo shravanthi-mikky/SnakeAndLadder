05808e499eef269f56311213150a40c110508a32
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-19 11:38:46 +0530

    rolling_dice to get a random num btwn 1 and 6!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Snake_and_Ladder;
+
 Console.WriteLine("Welcome to Snake and Ladder Simulator!!!");
+Uc1 test = new Uc1();
+Uc2 test2 = new Uc2();
+Console.WriteLine("Position of Player is Intiallly at : " +test.positionOfPlayer1);
+int current_position= test.positionOfPlayer1+ test2.RollingDice();
+Console.WriteLine(current_position);
---
+++ Uc1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder
+{
+    public class Uc1
+    {
+        public int positionOfPlayer1 = 0;
+    }
+}
